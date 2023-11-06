71ba172c1098819ecd83081e44778e3ccbf510a3
Nick Witten <nwitt12@gmail.com>
2023-11-06 08:09:49 -0500

    nlm.m as function

--- denoising/run_nlm.py
+++ denoising/run_nlm.py
@@ -3,7 +3,7 @@ import scipy
 from matlab import engine
 
 eng = engine.start_matlab()
-eng.nlm(nargout=0)
+eng.nlm("../images/cure-or-guassian-blur/01950.jpg", "data/01950_nlm.mat", nargout=0)
 
 data = scipy.io.matlab.loadmat("data/01950_nlm.mat")
 im_nl = data["im_nl"]
