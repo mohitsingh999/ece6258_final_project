3cf37bf35193faba65a8509775b6dd22d48a15fb
Nick Witten <nwitt12@gmail.com>
2023-11-06 07:56:29 -0500

    add denoising, images, paper

---
+++ denoising/run_nlm.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import scipy
+from matlab import engine
+
+eng = engine.start_matlab()
+eng.nlm(nargout=0)
+
+data = scipy.io.matlab.loadmat("data/01950_nlm.mat")
+im_nl = data["im_nl"]
+
+plt.imshow(im_nl, cmap="Greys")
+plt.show()
